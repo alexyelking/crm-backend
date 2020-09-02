d58542f92f574d2154a0c99a40e8f5d6dd9fcb30
Alexandr El'kin <newmanforlife@list.ru>
2020-09-02 14:51:08 +0600

    [FIX]Маленечко ещё SITE 11

--- app/app/Providers/RouteServiceProvider.php
+++ app/app/Providers/RouteServiceProvider.php
@@ -42,24 +42,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
         $this->mapApiAuthRoutes();
         $this->mapApiGuestRoutes();
-    }
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/api.php'));
     }
 
     /**
