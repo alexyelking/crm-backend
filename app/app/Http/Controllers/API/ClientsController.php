269d989326c78c619c9b006513abeab567f17462
Alexandr El'kin <newmanforlife@list.ru>
2020-09-03 15:29:06 +0600

    [FIX]SITE-17

--- app/app/Http/Controllers/API/ClientsController.php
+++ app/app/Http/Controllers/API/ClientsController.php
@@ -25,7 +25,7 @@ class ClientsController extends Controller
     public function update(UpdateRequest $request, Client $client)
     {
         $client->update($request->only(['name', 'email', 'phone']));
-        return Response::ok([$client]);
+        return Response::ok(["client"=>$client]);
     }
 
     public function destroy(Client $client)
@@ -37,6 +37,6 @@ class ClientsController extends Controller
     public function create(CreateRequest $request)
     {
         $client = Client::create($request->only(['name', 'email', 'phone']));
-        return Response::ok([$client]);
+        return Response::ok(["client"=>$client]);
     }
 }
