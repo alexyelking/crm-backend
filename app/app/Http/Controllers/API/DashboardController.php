07f1bf85b4e0522e719fbfdbc859820eb8c85039
Alexandr El'kin <newmanforlife@list.ru>
2020-09-03 15:28:02 +0600

    [FIX]SITE-14

--- app/app/Http/Controllers/API/DashboardController.php
+++ app/app/Http/Controllers/API/DashboardController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProgressResource;
 use Illuminate\Support\Facades\Response;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $a = mt_rand(1, 10)/10;
-        $b = mt_rand(70, 95)/100;
-        $c = mt_rand(3, 8)/10;
-        $d = mt_rand(10, 35)/100;
-        return Response::ok(["progress"=>["info" => "$a", "success" => "$b", "warning" => "$c", "danger" => "$d"]]);
+        return Response::ok(["progress" => new ProgressResource($this)]);
     }
 }
---
+++ app/app/Http/Resources/ProgressResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProgressResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "info"=>mt_rand(1, 10)/10,
+            "success"=>mt_rand(70, 95)/100,
+            "warning"=>mt_rand(3, 8)/10,
+            "danger"=>mt_rand(10, 35)/100,
+        ];
+    }
+}
