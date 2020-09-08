1f3e0a8149d90043fbf4392c94dbbf73bdc95515
Alexandr El'kin <newmanforlife@list.ru>
2020-09-08 20:55:29 +0600

    [FIX]Фиксик

--- app/app/Http/Controllers/API/ClientsController.php
+++ app/app/Http/Controllers/API/ClientsController.php
@@ -5,37 +5,29 @@ namespace App\Http\Controllers\API;
 use App\Http\Requests\Client\CreateRequest;
 use App\Http\Requests\Client\UpdateRequest;
 use App\Http\Resources\ClientMetaResource;
-use Illuminate\Http\Request;
+use App\Http\Resources\ClientDataResource;
 use Illuminate\Support\Facades\Response;
-use App\Http\Resources\ClientResource;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Client;
 
 class ClientsController extends Controller
 {
-    /*
-    public function index()
-    {
-        $clients = Client::query()->limit(100)->get();
-        return Response::ok(["data" => ClientResource::collection($clients)]);
-    }
-    */
-
     public function index(Request $request)
     {
         $clients = Client::paginate($request->limit);
-        return Response::ok(["data" => ClientResource::collection($clients), "meta" => new ClientMetaResource($clients)]);
+        return Response::ok(["data" => ClientDataResource::collection($clients), "meta" => new ClientMetaResource($clients)]);
     }
 
     public function show(Client $client)
     {
-        return Response::ok(["client" => new ClientResource($client)]);
+        return Response::ok(["client" => new ClientDataResource($client)]);
     }
 
     public function update(UpdateRequest $request, Client $client)
     {
         $client->update($request->only(['name', 'email', 'phone']));
-        return Response::ok(["client" => new ClientResource($client)]);
+        return Response::ok(["client" => new ClientDataResource($client)]);
     }
 
     public function destroy(Client $client)
@@ -47,6 +39,6 @@ class ClientsController extends Controller
     public function create(CreateRequest $request)
     {
         $client = Client::create($request->only(['name', 'email', 'phone']));
-        return Response::ok(["client" => new ClientResource($client)]);
+        return Response::ok(["client" => new ClientDataResource($client)]);
     }
 }
