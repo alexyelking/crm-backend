1f14299f2faf0d205be56b7784e0c7615be9d47d
Alexandr El'kin <newmanforlife@list.ru>
2020-09-10 12:46:09 +0600

    [DEV]SITE-28

--- app/app/Http/Controllers/API/DashboardController.php
+++ app/app/Http/Controllers/API/DashboardController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\API;
 use App\Http\Resources\ProgressResource;
 use Illuminate\Support\Facades\Response;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BarsResource;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return Response::ok(["progress" => new ProgressResource($this)]);
+        return Response::ok(["progress" => new ProgressResource($this), "bars" => new BarsResource($this)]);
     }
 }
